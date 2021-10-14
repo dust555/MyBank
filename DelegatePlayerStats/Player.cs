214082d89b77e0b3aad292c55eb5bbf610c1c90e
Joris Dieltiens <u0118137@thomasmore.be>
2021-10-14 14:07:41 +0200

    Delegate Playerstats

---
+++ DelegatePlayerStats/Player.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+
+namespace DelegatePlayerStats
+{
+
+    public delegate int GetNumber(Player p);
+
+    public class Player
+    {
+        public String Name { get; set; }
+        public int HeadShots { get; set; }
+        public int TotalScore { get; set; }
+        public int TimePlayed { get; set; }
+
+
+        // public static void MostTimePlayed(List<Player> playerlist){
+        //     Player winner = new();
+        //     int max = 0;
+        //     foreach(Player p in playerlist){
+        //         if(GetTimePlayed(p) > max){
+        //             winner = p;
+        //             max = p.TimePlayed;
+        //         }
+        //     }
+
+        //     System.Console.WriteLine($"{winner.Name} is the winner");
+        // }
+
+        //  public static void MostHeadshots(List<Player> playerlist){
+        //     Player winner = new();
+        //     int max = 0;
+        //     foreach(Player p in playerlist){
+        //         if(GetHeadshots(p) > max){
+        //             winner = p;
+        //             max = p.TimePlayed;
+        //         }
+        //     }
+
+        //     System.Console.WriteLine($"{winner.Name} is the winner");
+        // }
+
+        
+        //  public static void BestScore(List<Player> playerlist){
+        //     Player winner = new();
+        //     int max = 0;
+        //     foreach(Player p in playerlist){
+        //         if(GetTotalScore(p) > max){
+        //             winner = p;
+        //             max = p.TimePlayed;
+        //         }
+        //     }
+
+        //     System.Console.WriteLine($"{winner.Name} is the winner");
+        // }
+
+        public static void GetWinner(List<Player> playerlist, GetNumber getnum){
+            Player winner = new();
+            int max = 0;
+            foreach(Player p in playerlist){
+                if(getnum.Invoke(p) > max){
+                    winner = p;
+                    max = p.TimePlayed;
+                }
+            }
+
+            System.Console.WriteLine($"{winner.Name} is the winner");
+        }
+
+        public static int GetTotalScore(Player p){
+            return p.TotalScore;
+        }
+        public static int GetHeadshots(Player p){
+            return p.HeadShots;
+        }
+        public static int GetTimePlayed(Player p){
+            return p.TimePlayed;
+        }
+
+    }
+}
---
+++ DelegatePlayerStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatePlayerStats
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new();
+
+            players.Add(new Player(){Name="p1", TimePlayed=10, HeadShots=20, TotalScore=30});
+            players.Add(new Player(){Name="p2", TimePlayed=20, HeadShots=10, TotalScore=50});
+            players.Add(new Player(){Name="p3", TimePlayed=30, HeadShots=30, TotalScore=60});
+            
+            Player.GetWinner(players, Player.GetTimePlayed);
+
+        }
+    }
+}
